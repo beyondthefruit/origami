cfc7e23874fb9077daeee437c4fc7e2e0d4d8ebd
Kevin Hanard <kevin.hanard@gmail.com>
2022-12-19 15:14:16 +1100

    global Style creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import GlobalStyle from './globalStyles';
 import { useState } from 'react';
 
 function App() {
   return (
     <section>
+      <GlobalStyle />
       <h1>Origamax</h1>
     </section>
   );
