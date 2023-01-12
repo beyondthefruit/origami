5e85e1a97cbfef74826ff5a43be25eb86f44a119
Kevin Hanard <kevin.hanard@gmail.com>
2023-01-12 15:55:22 +1100

    feat: creation of banner

--- src/left/Presentation.style.js
+++ src/left/Presentation.style.js
@@ -6,3 +6,20 @@ export const PresentationContainer = styled.div`
   background-color: #777369;
   color: black;
 `;
+
+export const WebsiteBanner = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const WebsiteTitle = styled.div`
+  font-size: 2rem;
+  font-weight: 800;
+  width: auto;
+  margin: 1rem 0 0 1.5rem;
+`;
+export const WebsiteLogo = styled.img`
+  width: 32px;
+  height: 32px;
+  margin: 1rem 1.5rem 0 0;
+`;
